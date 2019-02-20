1244000e79dc4c7aee25c54534c99f8299f72c63
Szewah <swgrleong@gmail.com>
2019-02-19 21:41:42 -0500

    Fourth commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-var search = $(this).attr("data-headlines");
-var apikey = '3NHzwAZ0W4vY1w2LtontARaL9mG1GHDf';
-var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + search + apikey 
+var search = $("#term-input").val();
+// var apikey = '3NHzwAZ0W4vY1w2LtontARaL9mG1GHDf';
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=3NHzwAZ0W4vY1w2LtontARaL9mG1GHDf"
+console.log(queryURL);
+
+function displayArticles() {
+    $.ajax( {
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })    
+};
+
+displayArticles() 
+
+$("#search-button").on('click', function() {
+    event.preventDefault();
+    var searchResult = $("#term-input").val().trim();
+    console.log(search);
+});
