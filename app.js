014a3024f2d182b6f669cbc0b608de7dde3e186f
Szewah <swgrleong@gmail.com>
2019-02-20 16:11:43 -0500

    Sixth commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// $("#search-button").on("click", function() {
-// });
 $("button").on('click', function() {
 
     event.preventDefault()
-    var search = $("#term-input").val();
+    var searchTerms = $("#term-input").val();
+    // var startDate = $("#year-start").val();
+    // var endDate = $("#year-end").val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + 
-    search + "&api-key=3NHzwAZ0W4vY1w2LtontARaL9mG1GHDf";
+    searchTerms + "&api-key=3NHzwAZ0W4vY1w2LtontARaL9mG1GHDf";
     
     $.ajax({
         url: queryURL,
@@ -17,12 +17,18 @@ $("button").on('click', function() {
         var results = response.response.docs;
 
         for (var i = 0; i < results.length; i++) {
-            var resultsDiv = $("<div>");
-            var resultsHeadline = $("<p>");
-            resultsHeadline.text(results[i].headline.main);
-            var resultsImg = $("<img>");
-            resultsImg.attr('src', results[i].multimedia[2].web_url);
-            resultsDiv.append(resultsHeadline).append(resultsImg);
+            var resultsDiv = $("<div class='card col-md-4'>");
+            var resultsImg = $("<img class='card-img-top'>").attr("src", "https://static01.nyt.com/" + results[i].multimedia[0].url);
+            resultsImg.attr("alt", results[i].headline.main);
+            var resultsBody = $("<div class='card-body'>");
+            var resultsHeadline = $("<h6 class='title'>").text(results[i].headline.main);
+            var resultsSnippet = $("<p class='card-text'>").text(results[i].snippet);
+            var readMore = $("<a class='card-link btn btn-warning'>").text('Read more');
+            readMore.attr("href", results[i].web_url);
+            resultsBody.append(resultsHeadline);
+            resultsBody.append(resultsSnippet);
+            resultsBody.append(readMore);
+            resultsDiv.append(resultsImg).append(resultsBody);
             $("#search-results").append(resultsDiv);
         }
     })    
